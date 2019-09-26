1650e34d817b7e3c8c8152b012616db1f46b1ec7
Tamás Varga <mail@tvarga.hu>
2019-09-26 18:17:03 +0200

    initial import

---
+++ app/src/main/java/hu/tvarga/rates/MainActivity.java
@@ -0,0 +1,19 @@
+package hu.tvarga.rates;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hu.tvarga.rates.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction().replace(R.id.container,
+					MainFragment.newInstance()).commitNow();
+		}
+	}
+}
